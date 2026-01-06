62cbc958ba849aa60f08ce41d5c29835e678a8a3
arsn87 <taylan.aydin24@gmail.com>
2026-01-06 15:48:38 +0300

    Add navigation composables for task list and detail

--- app/src/main/java/com/yeditepe/finalexam/navigation/AppNavGraph.kt
+++ app/src/main/java/com/yeditepe/finalexam/navigation/AppNavGraph.kt
@@ -1,8 +1,13 @@
 package com.yeditepe.finalexam.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.yeditepe.finalexam.ui.TaskDetailScreen
+import com.yeditepe.finalexam.ui.TaskListScreen
 
 @Composable
 fun AppNavGraph() {
@@ -14,8 +19,13 @@ fun AppNavGraph() {
         startDestination = "tasks"
     ) {
 
-        // TODO 1: Add composable for "tasks"
+        composable("tasks") { TaskListScreen(navController = navController) }
 
-        // TODO 2: Add composable for "taskDetail/{title}"
+        composable(
+            route = "taskDetail/{title}",
+            arguments = listOf(navArgument("title") { type = NavType.StringType })
+        ) { backStackEntry ->
+            TaskDetailScreen(navController, title = backStackEntry.arguments?.getString("title"))
+        }
     }
 }
