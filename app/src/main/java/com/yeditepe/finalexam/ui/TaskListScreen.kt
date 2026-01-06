aa52da6bcc837f3411a795bafc258b277175d31c
arsn87 <taylan.aydin24@gmail.com>
2026-01-06 15:46:42 +0300

    Refactor TaskItemScreen to TaskListScreen with LazyColumn

--- app/src/main/java/com/yeditepe/finalexam/ui/TaskListScreen.kt
+++ app/src/main/java/com/yeditepe/finalexam/ui/TaskListScreen.kt
@@ -1,48 +1,34 @@
 package com.yeditepe.finalexam.ui
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import com.yeditepe.finalexam.model.Task
+import com.yeditepe.finalexam.viewmodel.TaskViewModel
 
 @Composable
-fun TaskItemScreen() {
+fun TaskListScreen(viewModel: TaskViewModel = viewModel(), navController: NavController) {
 
-    var isCompleted: Boolean MutableStateOf<false>
-
-    Column(modifier = Modifier.padding(16.dp)) {
-
-        Text(
-            text = "Submit Final Project",
-            style = MaterialTheme.typography.titleLarge
-        )
-
-        if (isCompleted) {
-            Text("Completed")
-        } else {
-            Text("Not Completed")
-        }
-        Button(
-            onClick = {
-                isCompleted = !isCompleted
-            }
-        ) {
-            Text("Change Status")
+    val tasks = viewModel.tasks
+    LazyColumn {
+        items(tasks) { task ->
+            TaskRow(task = task, navController = navController)
         }
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun TaskItemPreview() {
-    TaskItemScreen()
+fun TaskRow(task: Task, navController: NavController) {
+
+    Text(
+        text = "${task.title} - ${if (task.isCompleted) "Completed" else "Pending"}",
+        modifier = Modifier.clickable {
+            navController.navigate("taskDetail/${task.title}")
+        }
+    )
 }
