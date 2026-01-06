c43ab3123b965fc31c46f50e3cddddfb0931590d
arsn87 <taylan.aydin24@gmail.com>
2026-01-06 15:46:11 +0300

    Display task title in TaskDetailScreen
    Added a Text composable to display the task title.

--- app/src/main/java/com/yeditepe/finalexam/ui/TaskDetailScreen.kt
+++ app/src/main/java/com/yeditepe/finalexam/ui/TaskDetailScreen.kt
@@ -2,14 +2,18 @@ package com.yeditepe.finalexam.ui
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.yeditepe.finalexam.model.Task
 
 @Composable
 fun TaskDetailScreen(title: String) {
 
     Column(modifier = Modifier.padding(16.dp)) {
         // TODO 4: Display task title
+        Text(text = Task.title, style = MaterialTheme.typography.titleLarge)
     }
 }
